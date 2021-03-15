ebe9311a84992509f9f0e1bbfa8861570d9b89b1
Sindhu V Chalageri <sindhuvchalageri@gmail.com>
2021-03-15 11:32:12 +0530

    Update Trail.js
    update regarding filters and search bar

--- data/Trail.js
+++ data/Trail.js
@@ -1,13 +1,17 @@
+var data;
+var sin;
 fetch("Cosmetic.json")
 .then((response)=>{
    return response.json();
     
 })
-.then((data)=>{
-    var sin=data;
+.then((mydata)=>{
+    sin=mydata;
+    data=mydata;
     // document.getElementById("sindhu").innerHTML=data[0].id;
     // console.log(data[0]);
-    appendData(data);
+    // appendData(data);
+    display(data);
 })
 
 
@@ -39,34 +43,33 @@ function setId(id){
 }    
 
 
+
+
+
+
 function applyFilters(){
-    let b=document.getElementsByClassName("category");
     let categoryArray=[];
-    let colorArray=[];
+    
     let finishArray=[];
     let coverageArray=[];
-
+    
     let categoryListingArray=[];
-    let colorListingArray=[];
+    
     let finishListingArray=[];
     let coverageListingArray=[];
-
-  let mainArray=[];
-
-  let listingArray=[];
-
+    
+    let mainArray=[];
+    
+    let listingArray=[];
+    
+    let b=document.getElementsByClassName("category");
   for(let x of b){
       if(x.checked){
         categoryArray.push(x.value);
       }
   }
 
-  let c=document.getElementsByClassName("color");
-  for(let x of c){
-      if(x.checked){
-          colorArray.push(x.value);
-      }
-  }
+  
 
   let p=document.getElementsByClassName("finish");
   for(let x of p){
@@ -75,13 +78,13 @@ function applyFilters(){
       }
   }
 
-  let p=document.getElementsByClassName("coverage");
-  for(let x of p){
+  let cvg=document.getElementsByClassName("coverage");
+  for(let x of cvg){
       if(x.checked){
         coverageArray.push(x.value);
       }
   }
-  console.log(coverageArray);
+//   console.log(coverageArray);
 
   if(categoryArray.length!==0){
       for(let i=0;i<categoryArray.length;i++){
@@ -94,17 +97,7 @@ function applyFilters(){
       console.log(categoryListingArray);
   }
 
-  if(colorArray.length!==0)
-  {
-      for(let i=0;i<colorArray.length;i++){
-          for(let j=0;j<data.length;j++){
-             if(colorArray[i]===data[j].color){
-                 colorListingArray.push(data[j].id);
-            }
-          }
-      }
-      console.log(colorListingArray);
-  }
+  
 
   if(finishArray.length!==0){
       for(let i=0;i<finishArray.length;i++){
@@ -119,101 +112,113 @@ function applyFilters(){
       console.log(finishListingArray);
   }
 
-  if(categoryArray.length!==0){
-    for(let i=0;i<categoryArray.length;i++){
+  if(coverageArray.length!==0){
+    for(let i=0;i<coverageArray.length;i++){
         for(let j=0;j<data.length;j++){
-            if(categoryArray[i]===data[j].finish){
-              categoryListingArray.push(data[j].id);
+            if(coverageArray[i]===data[j].coverage){
+              coverageListingArray.push(data[j].id);
 
             }
            
         }
     }
-    console.log(categoryListingArray);
+    console.log(coverageListingArray);
 }
 // ------------------------------------ from here
+
   let temp=0;
 
-  if (categoryListingArray.length !== 0 && colorListingArray.length !== 0 && categoryListingArray.length!==0) {
+  if(categoryListingArray.length !== 0 && finishListingArray.length !== 0 && coverageListingArray.length!==0) {
       for(let i=0;i<categoryListingArray.length;i++){
-          for(let j=0;j<colorListingArray.length;j++){
-              for(let l=0;l<categoryListingArray.length;l++){
-              if((brandListingArray[i]==colorListingArray[j])&&(colorListingArray[j]==priceListingArray[l])){
+          for(let j=0;j<finishListingArray.length;j++){
+              for(let l=0;l<coverageListingArray.length;l++){
+              if((categoryListingArray[i]==finishListingArray[j])&&(finishListingArray[j]==coverageListingArray[l])){
                   for(let k=0;k<mainArray.length;k++){
-                      if(mainArray[k]===brandListingArray[i]){
+                      if(mainArray[k]===categoryListingArray[i]){
                           temp++;
                       }
                   }
-                  if(temp===0){
-                      mainArray.push(brandListingArray[i]);
+                  if(temp==0){
+                      mainArray.push(finishListingArray[j]);
                   }
+                  console.log("cat fin cov")
               }
           }
       }
       }
   }
-  else if (brandListingArray.length !== 0 && colorListingArray.length !== 0) {
-      for(let i=0;i<brandListingArray.length;i++){
-          for(let j=0;j<colorListingArray.length;j++){
-              if(brandListingArray[i]==colorListingArray[j]){
+  else if(categoryListingArray.length!==0 && finishListingArray.length!==0) {
+      console.log("inside");
+      for(let i=0;i<categoryListingArray.length;i++){
+          for(let j=0;j<finishListingArray.length;j++){
+              if(categoryListingArray[i]==finishListingArray[j]){
                   for(let k=0;k<mainArray.length;k++){
-                      if(mainArray[k]===brandListingArray[i]){
+                      if(mainArray[k]===categoryListingArray[i]){
                           temp++;
                       }
                   }
                   if(temp===0){
-                      mainArray.push(brandListingArray[i]);
+                      mainArray.push(categoryListingArray[i]);
                   }
+                  console.log("cat fin")
               }
           }
       }
   }
-  else if (brandListingArray.length !== 0 && priceListingArray.length !== 0) {
-      for(let i=0;i<brandListingArray.length;i++){
-          for(let j=0;j<priceListingArray.length;j++){
-              if(brandListingArray[i]==priceListingArray[j]){
+  else if(categoryListingArray.length !== 0 && coverageListingArray.length !== 0) {
+      for(let i=0;i<categoryListingArray.length;i++){
+          for(let j=0;j<coverageListingArray.length;j++){
+              if(categoryListingArray[i]==coverageListingArray[j]){
                   for(let k=0;k<mainArray.length;k++){
-                      if(mainArray[k]===brandListingArray[i]){
+                      if(mainArray[k]===categoryListingArray[i]){
                           temp++;
                       }
                   }
                   if(temp===0){
-                      mainArray.push(brandListingArray[i]);
+                      mainArray.push(categoryListingArray[i]);
                   }
+                  console.log("cat cov")
               }
           }
       }
   }
-  else if (priceListingArray.length !== 0 && colorListingArray.length !== 0) {
-      for(let i=0;i<priceListingArray.length;i++){
-          for(let j=0;j<colorListingArray.length;j++){
-              if(priceListingArray[i]==colorListingArray[j]){
+  else if(coverageListingArray.length !== 0 && finishListingArray.length !== 0) {
+      for(let i=0;i<coverageListingArray.length;i++){
+          for(let j=0;j<finishListingArray.length;j++){
+              if(coverageListingArray[i]==finishListingArray[j]){
                   for(let k=0;k<mainArray.length;k++){
-                      if(mainArray[k]===priceListingArray[i]){
+                      if(mainArray[k]===coverageListingArray[i]){
                           temp++;
                       }
                   }
                   if(temp===0){
-                      mainArray.push(priceListingArray[i]);
+                      mainArray.push(coverageListingArray[i]);
                   }
+                  console.log("fin cov")
               }
           }
       }
   }
-  else if(brandListingArray.length!==0){
-      for(let i=0;i<brandListingArray.length;i++){
-          mainArray.push(brandListingArray[i]);
+  else if(categoryListingArray.length!==0){
+      for(let i=0;i<categoryListingArray.length;i++){
+          mainArray.push(categoryListingArray[i]);
       }
+      console.log("cat")
   }
-  else if(colorListingArray.length!==0){
-      for(let i=0;i<colorListingArray.length;i++){
-          mainArray.push(colorListingArray[i]);
+  else if(finishListingArray.length!==0){
+      for(let i=0;i<finishListingArray.length;i++){
+          mainArray.push(finishListingArray[i]);
       }
+      console.log("fin")
   }
-  else if(priceListingArray.length!==0){
-      for(let i=0;i<priceListingArray.length;i++){
-          mainArray.push(priceListingArray[i]);
+  else if(coverageListingArray.length!==0){
+      for(let i=0;i<coverageListingArray.length;i++){
+          mainArray.push(coverageListingArray[i]);
       }
+      console.log("cov")
+  }
+  else{
+    console.log("empty")
   }
 
   console.log(mainArray);
@@ -223,17 +228,17 @@ function applyFilters(){
           if(mainArray[i]===e.id){
               return true;
           }
-      })
+      });
       listingArray.push(elem);
   }
   console.log(listingArray);
-  document.getElementById("mainId").innerHTML=""; 
+  document.getElementById("list").innerHTML=""; 
   display(listingArray);
   if(listingArray.length===0){
-      if(brandListingArray.length!==0 || colorListingArray.length!==0){
-      let res=document.getElementById("mainId");
+      if(categoryArray.length!==0 || finishArray.length!==0 || coverageArray.length!==0){
+      let res=document.getElementById("list");
       res.innerHTML="Product Not Found";
-      res.style.color="red";
+    //   res.style.color="red";
       res.style.fontWeight="bolder";
       res.style.fontSize="20px";
       }
@@ -241,13 +246,18 @@ function applyFilters(){
           display(data);
       }
   }
-     
+   
+}
+
+function display(newdata){
+    document.getElementById("list").innerHTML=""; 
+    appendData(newdata);
 }
 
 //-----------------------------------
 var input=document.getElementById("myInput");
 console.log(input);
-input.addEventListener('onKeyUp',(data)=>{
+input.addEventListener('keyUp',(data)=>{
     console.log(data);
 });
 
@@ -258,8 +268,11 @@ var searchbar=document.getElementById("myInput");
 searchbar.addEventListener('keyup', (e)=>{
     var searchString=e.target.value;
     var filteredCharacters=sin.filter((element)=>{
-        return element.name.includes(searchString);
+        let ele=element.name.toLowerCase()
+        return ele.includes(searchString);
         
     });
     console.log(filteredCharacters);
+    display(filteredCharacters)
+    
 })
